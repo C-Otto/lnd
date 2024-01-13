bbd8e2b6856d86479f442047ae13222e3e050153
Carsten Otto <git@c-otto.de>
2024-01-13 15:24:54 +0100

    htlcswitch: route via largest channel

--- htlcswitch/interfaces.go
+++ htlcswitch/interfaces.go
@@ -1,6 +1,7 @@
 package htlcswitch
 
 import (
+	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/channeldb/models"
@@ -119,6 +120,8 @@ type ChannelUpdateHandler interface {
 	// takes into account any forwarded but un-cleared HTLC's, and any
 	// HTLC's which have been set to the over flow queue.
 	Bandwidth() lnwire.MilliSatoshi
+
+	Capacity() btcutil.Amount
 
 	// EligibleToForward returns a bool indicating if the channel is able
 	// to actively accept requests to forward HTLC's. A channel may be
